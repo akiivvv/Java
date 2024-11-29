1d83f49e3024f0964255e536a9c84d9c9f887bb2
akiivvv <113914342+akiivvv@users.noreply.github.com>
2024-11-29 19:28:01 +0200

    Add files via upload

---
+++ pr 7.1/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] pyramid = {
+                {1},
+                {2, 3},
+                {4, 5, 6},
+                {7, 8, 9, 10}
+        };
+
+        System.out.println("Звичайний порядок:");
+        for (int[] row : pyramid) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nЗворотній порядок:");
+        for (int i = pyramid.length - 1; i >= 0; i--) {
+            for (int j = pyramid[i].length - 1; j >= 0; j--) {
+                System.out.print(pyramid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
